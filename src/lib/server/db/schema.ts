1d1e93a81a6b5689e5194e0265aee8f2f9d05d99
Maximilian Staiger <maximilian.staiger@web.de>
2026-02-28 00:55:20 +0100

    Add blog post table to db

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -1,1 +1,18 @@
+import { uuid, text, timestamp } from 'drizzle-orm/pg-core';
+import { pgTable } from 'drizzle-orm/pg-core';
+import { user } from './auth.schema';
+
 export * from './auth.schema';
+
+export const blogPost = pgTable('blog_post', {
+	id: uuid('id').defaultRandom().primaryKey(),
+	title: text('title').notNull(),
+	slug: text('slug').notNull().unique(),
+	content: text('content').notNull(),
+	authorId: text('author_id').references(() => user.id, { onDelete: "cascade" }).notNull(),
+	createdAt: timestamp("created_at").defaultNow().notNull(),
+	updatedAt: timestamp("updated_at")
+		.defaultNow()
+		.$onUpdate(() => /* @__PURE__ */ new Date())
+		.notNull(),
+});
