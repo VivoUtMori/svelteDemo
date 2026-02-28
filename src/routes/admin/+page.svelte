12dbe5f26c5ca6407ffb10fbe065814a05ca6f40
Maximilian Staiger <maximilian.staiger@web.de>
2026-02-28 21:52:10 +0100

    Make posts editable

--- src/routes/admin/+page.svelte
+++ src/routes/admin/+page.svelte
@@ -6,9 +6,12 @@
 	const formatDate = (date: Date) => {
 		return date.toLocaleDateString() + ' ' + date.toLocaleTimeString();
 	};
+
+	let editingPostId = $state<string | null>(null);
 </script>
 
-<h1>Blog Posts</h1>
+<h1>Blog Posts Admin</h1>
+<h4>Create New Post</h4>
 <form method="post" action="?/create">
 	<label>
 		Title
@@ -22,19 +25,42 @@
 </form>
 <p style="color: red">{form?.message ?? ''}</p>
 
+<h4>Manage Posts</h4>
 {#if data.blogPosts.length === 0}
 	<p>No blog posts found.</p>
 {:else}
 	<ul>
 		{#each data.blogPosts as post (post.id)}
 			<li>
-				<h3>{post.title}</h3>
-				<p><strong>Created:</strong> {formatDate(post.createdAt)}</p>
-				<p><strong>Author:</strong> {post.authorName}</p>
-				<p>{post.content}</p>
 				<form method="POST" action="?/delete">
 					<input type="hidden" name="id" value={post.id} />
-					<button>Delete</button>
+					{#if editingPostId === post.id}
+						<label style="margin-top: 1rem;">
+							Title
+							<input name="title" value={post?.title ?? ''} required />
+						</label>
+						<label>
+							Content
+							<textarea name="content" value={post?.content ?? ''} rows="6" required></textarea>
+						</label>
+						<button formaction="?/update">Save</button>
+						<button formaction="" onclick={() => (editingPostId = null)}>Cancel</button>
+					{:else}
+						<h5>{post.title}</h5>
+						<p>
+							<strong>Author:</strong>
+							{post.authorName}
+							&nbsp;
+							<strong>Created:</strong>
+							{formatDate(post.createdAt)}
+							&nbsp;
+							<strong>Updated:</strong>
+							{formatDate(post.updatedAt)}
+						</p>
+						<p>{post.content}</p>
+						<button formaction="" onclick={() => (editingPostId = post.id)}>Edit</button>
+						<button>Delete</button>
+					{/if}
 				</form>
 			</li>
 		{/each}
@@ -48,6 +74,10 @@
 
 	ul li + li {
 		border-top: 1px solid var(--totl-primate-font-descolor);
-		margin-top: 3rem;
+		margin-top: 2rem;
+	}
+
+	h5 {
+		margin: 1rem 0 0.5rem;
 	}
 </style>
