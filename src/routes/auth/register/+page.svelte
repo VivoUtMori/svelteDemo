d24d5d2b83ad2cffaa16593530c4b526173e9f35
Maximilian Staiger <maximilian.staiger@web.de>
2026-02-28 01:59:51 +0100

    Add required tag to fields in register form

--- src/routes/auth/register/+page.svelte
+++ src/routes/auth/register/+page.svelte
@@ -9,19 +9,19 @@
 <form method="post" action="?/signUpEmail" use:enhance>
 	<label>
 		Name
-		<input name="name" value={form?.name ?? ''} />
+		<input name="name" value={form?.name ?? ''} required />
 	</label>
 	<label>
 		Email
-		<input type="email" name="email" value={form?.email ?? data.email ?? ''} />
+		<input type="email" name="email" value={form?.email ?? data.email ?? ''} required />
 	</label>
 	<label>
 		Password
-		<input type="password" name="password" />
+		<input type="password" name="password" required />
 	</label>
 	<label>
 		Confirm Password
-		<input type="password" name="confirmPassword" />
+		<input type="password" name="confirmPassword" required />
 	</label>
 	<button>Register</button>
 </form>
